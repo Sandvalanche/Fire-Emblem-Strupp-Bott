df39287024fda0f05539faec2b7086a8f197d116
Jacob Strupp <str12045@byui.edu>
2016-05-12 13:45:27 -0600

    5/12 - Finish player class

--- fireEmblem/src/byui/cit260/almostFireEmblem/model/Player.java
+++ fireEmblem/src/byui/cit260/almostFireEmblem/model/Player.java
@@ -5,10 +5,73 @@
  */
 package byui.cit260.almostFireEmblem.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author Carson
  */
-public class Player {
+public class Player implements Serializable {
+
+    private String name;
+    private double bestTime;
+
+    public Player() {       //constructor function
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBestTime() {
+        return bestTime;
+    }
+
+    public void setBestTime(double bestTime) {
+        this.bestTime = bestTime;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }   
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", bestTime=" + bestTime + '}';
+    }
     
 }
+
+
+
